c85e2e06416cda89edaeed181ca758ff96d282b0
aletsislm10 <141757684+aletsislm10@users.noreply.github.com>
2023-08-08 11:06:12 -0600

    Add files via upload

---
+++ Models/Cerveza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosCSharp.Models
+{
+    public class Cerveza : Bebida
+    {
+        /*public Cerveza() :base("cerveza",1000){ }*/
+
+        //cuando se haga una cerveza se va a pedir obligado la cantidad y el nombre opcional
+        public Cerveza (int Cantidad, string Nombre = "Cerveza") : base (Nombre,Cantidad) { }
+    }
+}
--- tutoriales/Program.cs
+++ tutoriales/Program.cs
@@ -9,21 +9,16 @@ namespace FundamentosCSharp
     internal class Program
     {
         static void Main(string[] args)
-        {  
+        {
             Bebida bebida = new Bebida("CocaCola",1000);
             bebida.Nombre = "Coca Cola";
             bebida.Beberse(500);
             Console.WriteLine(bebida.Cantidad);
-
-            /*Cerveza cerveza = new Cerveza();
+            
+            Cerveza cerveza = new Cerveza();
             cerveza.Beberse(100);
             Console.WriteLine(cerveza.Cantidad);
-            */
 
-            Cerveza cerveza = new Cerveza(500);
-            cerveza.Beberse(100);
-            Console.WriteLine(cerveza.Cantidad); 
-            
             Console.ReadLine();
         }
     }
