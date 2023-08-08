db5bfba1cdffecf3c0f2f8540c3b47975cb5c403
aletsislm10 <141757684+aletsislm10@users.noreply.github.com>
2023-08-08 10:11:48 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FundamentosCSharp.Models;
+namespace FundamentosCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bebida bebida = new Bebida("CocaCola",1000);
+            bebida.Nombre = "Coca Cola";
+            bebida.Beberse(500);
+            Console.WriteLine(bebida.Cantidad);
+            
+            Cerveza cerveza = new Cerveza();
+            cerveza.Beberse(100);
+            Console.WriteLine(cerveza.Cantidad);
+
+            Console.ReadLine();
+        }
+    }
+}
